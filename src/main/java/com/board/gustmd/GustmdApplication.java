9518c251ee433dac203a1aeac2df7641ec4adcbb
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-17 09:36:44 +0900

    :tada: 프로젝트 재생성

---
+++ src/main/java/com/board/gustmd/GustmdApplication.java
@@ -0,0 +1,13 @@
+package com.board.gustmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GustmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/gustmd/GustmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.gustmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
