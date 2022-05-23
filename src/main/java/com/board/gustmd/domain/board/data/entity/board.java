abbf267a06faa1681e6721b44c74cb76d09517b9
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-23 15:03:54 +0900

    :sparkles: board dto 생성

--- src/main/java/com/board/gustmd/domain/board/data/entity/board.java
+++ src/main/java/com/board/gustmd/domain/board/data/entity/board.java
@@ -1,4 +1,21 @@
 package com.board.gustmd.domain.board.data.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String userName;
+
+    @Column
+    private String description;
 }
