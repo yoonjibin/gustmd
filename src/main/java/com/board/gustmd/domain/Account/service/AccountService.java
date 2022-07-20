bbc5e3daa45e7168fe05de40a893f7ea37eec53b
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-07-20 20:58:00 +0900

    add service interface

--- src/main/java/com/board/gustmd/domain/Account/service/AccountService.java
+++ src/main/java/com/board/gustmd/domain/Account/service/AccountService.java
@@ -1,4 +1,6 @@
 package com.board.gustmd.domain.Account.service;
 
 public interface AccountService {
+    public void login();
+    public void
 }
--- src/main/java/com/board/gustmd/domain/Account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/board/gustmd/domain/Account/service/impl/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package com.board.gustmd.domain.Account.service.impl;
 
 import com.board.gustmd.domain.Account.service.AccountService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AccountServiceImpl implements AccountService {
 }
