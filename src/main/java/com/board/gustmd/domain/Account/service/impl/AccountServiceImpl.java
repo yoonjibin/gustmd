b8a94c05ef973bdf32c8670f40193b499bb28d9b
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-07-29 22:05:57 +0900

    add exception

--- src/main/java/com/board/gustmd/domain/Account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/board/gustmd/domain/Account/service/impl/AccountServiceImpl.java
@@ -4,6 +4,7 @@ import com.board.gustmd.domain.Account.data.dto.request.RegisterRequest;
 import com.board.gustmd.domain.Account.service.AccountService;
 import com.board.gustmd.domain.user.data.entity.User;
 import com.board.gustmd.domain.user.repository.UserRepository;
+import com.board.gustmd.global.user.utill.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
@@ -15,11 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccountServiceImpl implements AccountService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserUtil userUtil;
 
     @Override
     @Transactional
     public void register(RegisterRequest registerRequest) {
-
+        userUtil.checkExistsEmail(registerRequest.getEmail());
         User user=registerRequest.toEntity(passwordEncoder.encode(registerRequest.getPassword()));
         userRepository.save(user);
     }
