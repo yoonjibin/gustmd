5cf650995364be19aaa610fca985bc1160502c35
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-08-06 21:54:10 +0900

    add userDetails

--- src/main/java/com/board/gustmd/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/board/gustmd/global/security/jwt/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.board.gustmd.global.security.jwt;
 
+import com.board.gustmd.global.security.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
-    
+    private final JwtProperties jwtProperties;
+
 
 }
