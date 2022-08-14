94c8604e5502a5c41c5d6c8d5f4ab7f0d8ce7d8a
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-08-14 13:35:02 +0900

    add TokenResponse

---
+++ src/main/java/com/board/gustmd/domain/Account/data/dto/response/TokenResponse.java
@@ -0,0 +1,13 @@
+package com.board.gustmd.domain.Account.data.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+    private final String refreshToken;
+    private final String accessExp;
+    private final String refreshExp;
+}
